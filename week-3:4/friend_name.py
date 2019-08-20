6e5766b8f5d64aefd4e7dc6c21fee46ce03dc193
Ayo <gudmuv@gmail.com>
2019-08-19 19:37:36 -0500

    Update friend_name.py

--- week-3:4/friend_name.py
+++ week-3:4/friend_name.py
@@ -1,3 +1,6 @@
+#Make a program that filters a list of strings and returns a list with only your friends names in it. If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure it’s not…
+#Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
 def friends_list(names):
     new_names = []
 
@@ -5,8 +8,6 @@ def friends_list(names):
         if len(i) == 4:
             new_names.append(i)
     return new_names
-
-
 
 
 names = (input("Enter friends' name separated by a comma: ")).split(",")
